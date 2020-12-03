aa763e192ea3ed6e983491f88caa3a8a4568de30
Ram Subramanian <needmoarram@gmail.com>
2020-12-02 22:32:54 -0800

    p3: input, py

---
+++ problem-3.py
@@ -0,0 +1,22 @@
+with open('input-3.txt') as f:
+    world = f.read().splitlines()
+
+def use_slope(rt, dn):
+    trees_hit = 0
+    i = dn
+    j = rt
+    while i < len(world):
+        if world[i][j] == '#':
+            trees_hit += 1
+        i += dn
+        j = (j + rt) % len(world[0])
+    return trees_hit
+
+ans = use_slope(3, 1)
+print("part 1:", ans)
+
+ans *= use_slope(1, 1)
+ans *= use_slope(5, 1)
+ans *= use_slope(7, 1)
+ans *= use_slope(1, 2)
+print("part 2:", ans)
