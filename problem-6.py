0dffd5c46c0abc54ab9afe6b384cb0c354d3222a
Ram Subramanian <needmoarram@gmail.com>
2020-12-08 19:11:56 -0800

    p6: input, py

---
+++ problem-6.py
@@ -0,0 +1,14 @@
+with open('input-6.txt') as f:
+    groups = f.read().split('\n\n')
+
+sum_any = 0
+sum_all = 0
+for group in groups:
+    g = group.split('\n')
+    sl = set(''.join(g))
+    sum_any += len(sl)
+    for p in g:
+        sl &= set(p)
+    sum_all += len(sl)
+print("part 1:", sum_any)
+print("part 2:", sum_all)
