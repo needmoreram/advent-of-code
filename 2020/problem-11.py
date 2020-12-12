ceff7f1ced251c311d5e6c69b97fa0effb8a51e6
Ram Subramanian <needmoarram@gmail.com>
2020-12-11 18:07:53 -0800

    p11: py-part1 even better

--- 2020/problem-11.py
+++ 2020/problem-11.py
@@ -2,22 +2,21 @@ import numpy as np
 from scipy.ndimage import convolve
 
 with open('input-11.txt') as f:
-    grid = map(list, f.read().splitlines())
-    grid = np.array(list(grid))
-    ng = np.zeros(grid.shape, np.int)
+    g = map(list, f.read().splitlines())
+    g = np.array(list(g))
+    dots = np.where(g == '.')
+    grid = np.zeros(g.shape, np.int)
 
 k = np.ones((3, 3))
 k[1, 1] = 0
 
 while True:
-    c = convolve(ng, k, mode='constant', cval = 0)
-    c[np.where(grid == '.')] = -1
+    c = convolve(grid, k, mode='constant', cval = 0)
+    c[dots] = -1
     vacate = np.where(c >= 4)
     place = np.where(c == 0)
-    if np.all(ng[vacate] == 0) and np.all(ng[place] == 1):
+    if np.all(grid[vacate] == 0) and np.all(grid[place] == 1):
         break
-    grid[vacate] = 'L'
-    ng[vacate] = 0
-    grid[place] = '#'
-    ng[place] = 1
-print("part 1:", len(grid[grid == '#']))
+    grid[vacate] = 0
+    grid[place] = 1
+print("part 1:", len(grid[grid == 1]))
