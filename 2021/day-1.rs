c8d3497608c06dad4860470c20b41d97b00f67e8
Ram Subramanian <ram@posteo.net>
2021-12-20 17:14:48 -0800

    2021-day-1

---
+++ 2021/day-1.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::VecDeque;
+
+fn main() {
+  let file = File::open("input/day-1.txt").unwrap();
+  let reader = BufReader::new(file);
+
+  let mut window: VecDeque<i32> = VecDeque::new();
+  let mut running_sum: i32 = 0;
+  let mut last_num: i32 = std::i32::MAX;
+  let mut ans_p1: u32 = 0;
+  let mut ans_p2: u32 = 0;
+
+  for line in reader.lines() {
+    let num: i32 = line.unwrap().parse().unwrap();
+    if window.len() >= 3 {
+      let old_sum = running_sum;
+      running_sum = running_sum - window.pop_front().unwrap() + num;
+      if running_sum > old_sum {
+        ans_p2 += 1;
+      }
+    }
+    window.push_back(num);
+    if num > last_num {
+      ans_p1 += 1;
+    }
+    last_num = num;
+  }
+
+  println!("{}", ans_p1);
+  println!("{}", ans_p2);
+}
