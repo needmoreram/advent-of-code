d2301864e875a71887820d5d088c305920b1217d
Ram Subramanian <ram@posteo.net>
2022-06-26 00:30:43 -0700

    2021: rewrite day-1.rs

--- 2021/day-1.rs
+++ 2021/day-1.rs
@@ -1,38 +1,14 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::collections::VecDeque;
+fn part1(depths: &[u16]) -> usize {
+  return depths.windows(2).filter(|v| v[0] < v[1]).count();
+}
 
-fn _attempt_1() {
-  let file = File::open("input/day-1.txt").unwrap();
-  let reader = BufReader::new(file);
-
-  let mut window: VecDeque<i32> = VecDeque::new();
-  let mut running_sum: i32 = 0;
-  let mut last_num: i32 = std::i32::MAX;
-  let mut ans_p1: u32 = 0;
-  let mut ans_p2: u32 = 0;
-
-  for line in reader.lines() {
-    let num: i32 = line.unwrap().parse().unwrap();
-    if window.len() >= 3 {
-      let old_sum = running_sum;
-      running_sum = running_sum - window.pop_front().unwrap() + num;
-      if running_sum > old_sum {
-        ans_p2 += 1;
-      }
-    }
-    window.push_back(num);
-    if num > last_num {
-      ans_p1 += 1;
-    }
-    last_num = num;
-  }
-
-  println!("{}", ans_p1);
-  println!("{}", ans_p2);
+fn part2(depths: &[u16]) -> usize {
+  return depths.windows(4).filter(|v| v[0] < v[3]).count();
 }
 
 fn main() {
   let lines = include_str!("input/day-1.txt").lines();
-  let rest = lines.map(|n| n.parse().unwrap()).collect::<Vec<u16>>();
+  let depths = lines.map(|n| n.parse().unwrap()).collect::<Vec<u16>>();
+  println!("{}", part1(&depths));
+  println!("{}", part2(&depths));
 }
